4fb9770af360a92d827bea0882727601c86f535e
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2021-10-17 23:47:28 +0300

    Re-factor for more diciplined approach. * still need array paradigm approach.

--- largest-series-product.R
+++ largest-series-product.R
@@ -1,9 +1,9 @@
 library("purrr")
 
-# supress warnings until I find a way to solve them:
+                                        # supress warnings until I find a way to solve them:
 options(warn=-1)
 
-# definitions of problem space:
+                                        # definitions of problem space:
 identity <- function(a) a
 is_done <- function(a) (class(a) == "rlang_box_done")
 wrap_if_done <- function(f) function(v) if(is_done(v)) v else f(v)
@@ -15,10 +15,34 @@ indices <- function(span) function(ns) 1:(length(ns) - span + 1)
 value_and_indices <- function(span) function(v) map(c(identity, (indices(span))), function(f) f(v))
 squence_of_prod_f <- function(span) function(ns) function(a) prod(ns[seq(a, length.out = span)])
 
-# aliases:
-ff <- wrap_if_done # a simple alias for long call.
-
-# problem function:
+                                        # problem function:
 largest_series_product <- function(digits, span){
-  digits %>% strsplit(., NULL) %>% unlist %>% as.numeric %>% error_on_any_na %>% (error_on_span_less_than_zero(span)) %>% (return_one_if_span_zero(span)) %>% (ff(value_and_indices(span))) %>% (ff(function(vi) list(squence_of_prod_f(span)(vi[[1]]), vi[[2]]))) %>% (ff(function(vp) sapply(vp[[2]], vp[[1]]))) %>% (ff(function(a) max(a))) %>% unwrap_done
+  to_numeric_list <- c(
+    function(a) strsplit(a, NULL),
+    unlist,
+    as.numeric
+  )
+
+  error_control <- c(
+    error_on_any_na,
+    (error_on_span_less_than_zero(span)),
+    (value_and_indices(span))
+  )
+
+  calculate_LSP <- c(
+    function(vi) list(squence_of_prod_f(span)(vi[[1]]), vi[[2]]),
+    function(vp) sapply(vp[[2]], vp[[1]]),
+    function(a) max(a)
+    )
+
+  boxed_calculate_LSP <- map(calculate_LSP, wrap_if_done)
+
+  LSP <- c(
+    to_numeric_list,
+    error_control,
+    boxed_calculate_LSP,
+    unwrap_done
+  )
+
+  reduce(.x=LSP, .f=function(acc, f) f(acc), .init=digits, .dir="forward")
 }
